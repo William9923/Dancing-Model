c0f1986797f350d71cf2d385495cbe7e6b4410f0
naufal-dean <13518123@std.stei.itb.ac.id>
2021-04-05 13:02:52 +0700

    chore: create new app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import Shape from "./shapes/shape";
+import Scene from "./scene";
 import SliderManager from "./SliderManager";
 
 const X = 0;
@@ -6,108 +6,33 @@ const Y = 1;
 const Z = 2;
 
 class App {
-  private shape: Shape | null = null;
+  private scene: Scene | null = null;
 
-  constructor() {}
-
-  initSliders() {
-    if (!this.shape) {
-      return;
-    }
-
-    const changeRotateOf = (index: number, degree: number) => {
-      if (!this.shape) {
-        return;
-      }
-      const newRotation = this.shape.getTransformation("rotate");
-      newRotation[index] = degree;
-      this.shape.setTransformation("rotate", newRotation);
-    };
-    const changeTranslateOf = (index: number, val: number) => {
-      if (!this.shape) {
-        return;
-      }
-      const newVal = this.shape.getTransformation("translate");
-      newVal[index] = (val - 50) / 100;
-      this.shape.setTransformation("translate", newVal);
-    };
-    const changeZoomOf = (val: number) => {
-      if (!this.shape) {
-        return;
-      }
-      this.shape.setTransformation("scale", [val, val, val]);
-    };
-    const changeCameraPositionOf = (cameraSettingType: CameraSetting, newValue: number) => {
-      this.shape?.setCamera(cameraSettingType, newValue);
-    };
-
-    SliderManager.assignInputEvent("rotate-x", (val: number) => {
-      changeRotateOf(X, val);
-    });
-    SliderManager.assignInputEvent("rotate-y", (val: number) => {
-      changeRotateOf(Y, val);
-    });
-    SliderManager.assignInputEvent("rotate-z", (val: number) => {
-      changeRotateOf(Z, val);
-    });
-    SliderManager.assignInputEvent("x", (val: number) => {
-      changeTranslateOf(X, val);
-    });
-    SliderManager.assignInputEvent("y", (val: number) => {
-      changeTranslateOf(Y, val);
-    });
-    SliderManager.assignInputEvent("z", (val: number) => {
-      changeTranslateOf(Z, val);
-    });
-    SliderManager.assignInputEvent("zoom", (val: number) => {
-      changeZoomOf(val);
-    });
-    SliderManager.assignInputEvent("cam-radius", (val: number) => {
-      changeCameraPositionOf("radius", val);
-    });
-    SliderManager.assignInputEvent("cam-theta", (val: number) => {
-      changeCameraPositionOf("theta", val);
-    });
-    SliderManager.assignInputEvent("cam-phi", (val: number) => {
-      changeCameraPositionOf("phi", val);
-    });
+  constructor(scene: Scene) {
+    this.scene = scene;
   }
 
-  start() {
-    if (!this.shape) {
-      throw "No shape defined!";
+  public setScene(scene: Scene) {
+    this.scene = scene;
+  }
+
+  public setSceneProjection(projectionType: Projection) {
+    this.scene?.setProjection(projectionType);
+  }
+
+  public toggleShading(useShading: boolean) {
+    this.scene?.setUseShading(useShading);
+  }
+
+  public start() {
+    if (!this.scene) {
+      throw "No scene defined!";
     }
-    this.initSliders();
     const loop = (time: number) => {
-      this.shape?.draw();
+      this.scene?.render();
       window.requestAnimationFrame(loop);
-    };
+    }
     window.requestAnimationFrame(loop);
-  }
-
-  setShape(shape: Shape) {
-    this.shape = shape;
-  }
-
-  resetShape() {
-    SliderManager.resetTransformSliderValue();
-  }
-
-  resetCamera() {
-    SliderManager.resetCameraSliderValue();
-  }
-
-  resetAll() {
-    this.resetShape();
-    this.resetCamera();
-  }
-
-  setShapeProjection(projectionType: Projection) {
-    this.shape?.setProjection(projectionType);
-  }
-
-  toggleShading(useShading: boolean) {
-    this.shape?.setUseShading(useShading);
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,16 @@
 import App from "./app";
-import TriangularPrism from "./shapes/triangularPrism";
+import TriangularPrism from "./object/triangularPrism";
 import Shape from "./shapes/shape";
+import Light from "./light";
+import Camera from "./camera";
+import Scene from "./scene";
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
-// function for debugging block / cube as well
-function initShape(shapeName: ShapeType): Shape {
-  let obj: Shape;
-  switch (shapeName) {
-    case "prism":
-      obj = new TriangularPrism(canvas);
-      break;
-  }
-  const shadingElmt = document.getElementById("shading") as HTMLInputElement;
-  obj.setUseShading(shadingElmt.checked);
-  return obj;
-}
+const scene = new Scene(canvas);
+scene.add(new TriangularPrism());
 
-// Init default shapes
-// change this to prism later
-const defaultObj = initShape("prism");
-
-// Init app
-const app = new App();
-app.setShape(defaultObj);
+const app = new App(scene);
 
 // Pick hollow object buttons event handler
 const prismBtn = document.getElementById("prism") as HTMLElement;
@@ -44,36 +31,83 @@ blockBtn.addEventListener("click", () => {
   app.setShape(initShape("block"));
 });
 
-// Perspective buttons event handler
-const orthographicBtn = document.getElementById("orthographic") as HTMLElement;
-orthographicBtn.addEventListener("click", () => {
-  app.setShapeProjection("orthographic");
-});
-
-const obliqueBtn = document.getElementById("oblique") as HTMLElement;
-obliqueBtn.addEventListener("click", () => {
-  app.setShapeProjection("oblique");
-});
-
-const perspectiveBtn = document.getElementById("perspective") as HTMLElement;
-perspectiveBtn.addEventListener("click", () => {
-  app.setShapeProjection("perspective");
-});
-
-// Reset button event handler
-const resetBtn = document.getElementById("reset") as HTMLElement;
-resetBtn.addEventListener("click", () => {
-  app.resetShape();
-});
-
-const camResetBtn = document.getElementById("cam-reset") as HTMLElement;
-camResetBtn.addEventListener("click", () => {
-  app.resetCamera();
-});
-
 const shadingToggle = document.getElementById("shading") as HTMLInputElement;
 shadingToggle.addEventListener("change", () => {
   app.toggleShading(shadingToggle.checked);
 });
-// Start app
+
 app.start();
+
+// // function for debugging block / cube as well
+// function initShape(shapeName: ShapeType): Shape {
+//   let obj: Shape;
+//   switch (shapeName) {
+//     case "prism":
+//       obj = new TriangularPrism(canvas);
+//       break;
+//   }
+//   const shadingElmt = document.getElementById("shading") as HTMLInputElement;
+//   obj.setUseShading(shadingElmt.checked);
+//   return obj;
+// }
+//
+// // Init default shapes
+// // change this to prism later
+// const defaultObj = initShape("prism");
+//
+// // Init app
+// const app = new App();
+// app.setShape(defaultObj);
+//
+// // Pick hollow object buttons event handler
+// const prismBtn = document.getElementById("prism") as HTMLElement;
+// prismBtn.addEventListener("click", () => {
+//   app.resetAll();
+//   app.setShape(initShape("prism"));
+// });
+//
+// const cubeBtn = document.getElementById("cube") as HTMLElement;
+// cubeBtn.addEventListener("click", () => {
+//   app.resetAll();
+//   app.setShape(initShape("cube"));
+// });
+//
+// const blockBtn = document.getElementById("block") as HTMLElement;
+// blockBtn.addEventListener("click", () => {
+//   app.resetAll();
+//   app.setShape(initShape("block"));
+// });
+//
+// // Perspective buttons event handler
+// const orthographicBtn = document.getElementById("orthographic") as HTMLElement;
+// orthographicBtn.addEventListener("click", () => {
+//   app.setShapeProjection("orthographic");
+// });
+//
+// const obliqueBtn = document.getElementById("oblique") as HTMLElement;
+// obliqueBtn.addEventListener("click", () => {
+//   app.setShapeProjection("oblique");
+// });
+//
+// const perspectiveBtn = document.getElementById("perspective") as HTMLElement;
+// perspectiveBtn.addEventListener("click", () => {
+//   app.setShapeProjection("perspective");
+// });
+//
+// // Reset button event handler
+// const resetBtn = document.getElementById("reset") as HTMLElement;
+// resetBtn.addEventListener("click", () => {
+//   app.resetShape();
+// });
+//
+// const camResetBtn = document.getElementById("cam-reset") as HTMLElement;
+// camResetBtn.addEventListener("click", () => {
+//   app.resetCamera();
+// });
+//
+// const shadingToggle = document.getElementById("shading") as HTMLInputElement;
+// shadingToggle.addEventListener("change", () => {
+//   app.toggleShading(shadingToggle.checked);
+// });
+// // Start app
+// app.start();
