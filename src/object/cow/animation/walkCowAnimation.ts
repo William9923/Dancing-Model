6207b8d738613033e9f91307e9a37fca74375a59
Vincentius Lienardo <vincentiuslienardo@gmail.com>
2021-04-21 21:12:13 +0700

    finishing complete, cow walked beautifully

--- src/object/cow/animation/walkCowAnimation.ts
+++ src/object/cow/animation/walkCowAnimation.ts
@@ -5,35 +5,45 @@ const animation = {
     {
       index: 0,
       position: {
-        head: -10,
+        head: -5,
         lfl: 10,
         rfl: -10,
         lbl: -10,
         rbl: 10
       },
     },
-    {
-      index: 25,
-      position: {
-        head: 0,
-        lfl: 0,
-        rfl: 0,
-        lbl: 0,
-        rbl: 0
-      },
-    },
+    // {
+    //   index: 25,
+    //   position: {
+    //     head: 0,
+    //     lfl: 0,
+    //     rfl: 0,
+    //     lbl: 0,
+    //     rbl: 0
+    //   },
+    // },
     {
       index: 50,
       position: {
-        head: 10,
+        head: 5,
         lfl: -10,
         rfl: 10,
         lbl: 10,
         rbl: -10
       },
     },
+    // {
+    //   index: 75,
+    //   position: {
+    //     head: 0,
+    //     lfl: 0,
+    //     rfl: 0,
+    //     lbl: 0,
+    //     rbl: 0
+    //   },
+    // },
     {
-      index: 75,
+      index: 100,
       position: {
         head: 0,
         lfl: 0,
@@ -42,18 +52,8 @@ const animation = {
         rbl: 0
       },
     },
-    {
-      index: 100,
-      position: {
-        head: -10,
-        lfl: 50,
-        rfl: -50,
-        lbl: -50,
-        rbl:50
-      },
-    },
   ],
-  duration: 5,
+  duration: 1.5,
 };
 
 class WalkCowAnimation extends CowAnimation {
